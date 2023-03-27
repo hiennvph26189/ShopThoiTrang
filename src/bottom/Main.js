b555b20248be83c32e82c037dda9923eace5f909
saonguyen <jorrynguyen@gmail.com>
2023-03-27 12:55:33 +0700

    a

---
+++ src/bottom/Main.js
@@ -0,0 +1,14 @@
+import { Text, View } from "react-native";
+import React from "react";
+import Header from "../common/Header";
+const Main = () => {
+    return (
+        <View style={{flex:1}}>
+            <Header
+                title={'Home'}
+                
+            />
+        </View>
+    )
+}
+export default Main;
